5aa23e1057bbccdb6eb2b67a8894a177570ba81a
Igor <northwatch@thenorthwatch.ca>
2026-07-02 23:41:52 +0000

    Add missing star sparkle beside maple leaf in logo mark

--- nav.js
+++ nav.js
@@ -25,7 +25,7 @@
     <nav class="site-nav" id="site-nav">
       <div class="site-nav-inner">
         <a class="site-nav-brand" href="${r}index.html" id="site-nav-brand">
-          <span id="site-nav-brand-word">NORTHWATCH</span><span class="site-nav-brand-leaf" id="site-nav-brand-leaf"><svg viewBox="315.5 95 127.8 279" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M201 232l-13.3 4.4 61.4 54c4.7 13.7-1.6 17.8-5.6 25l66.6-8.4-1.6 67 13.9-.3-3.1-66.6 66.7 8c-4.1-8.7-7.8-13.3-4-27.2l61.3-51-10.7-4c-8.8-6.8 3.8-32.6 5.6-48.9 0 0-35.7 12.3-38 5.8l-9.2-17.5-32.6 35.8c-3.5.9-5-.5-5.9-3.5l15-74.8-23.8 13.4q-3.2 1.3-5.2-2.2l-23-46-23.6 47.8q-2.8 2.5-5 .7L264 130.8l13.7 74.1c-1.1 3-3.7 3.8-6.7 2.2l-31.2-35.3c-4 6.5-6.8 17.1-12.2 19.5s-23.5-4.5-35.6-7c4.2 14.8 17 39.6 9 47.7Z"/></svg></span>
+          <span id="site-nav-brand-word">NORTHWATCH</span><span class="site-nav-brand-leaf" id="site-nav-brand-leaf"><svg viewBox="315.5 95 127.8 279" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M201 232l-13.3 4.4 61.4 54c4.7 13.7-1.6 17.8-5.6 25l66.6-8.4-1.6 67 13.9-.3-3.1-66.6 66.7 8c-4.1-8.7-7.8-13.3-4-27.2l61.3-51-10.7-4c-8.8-6.8 3.8-32.6 5.6-48.9 0 0-35.7 12.3-38 5.8l-9.2-17.5-32.6 35.8c-3.5.9-5-.5-5.9-3.5l15-74.8-23.8 13.4q-3.2 1.3-5.2-2.2l-23-46-23.6 47.8q-2.8 2.5-5 .7L264 130.8l13.7 74.1c-1.1 3-3.7 3.8-6.7 2.2l-31.2-35.3c-4 6.5-6.8 17.1-12.2 19.5s-23.5-4.5-35.6-7c4.2 14.8 17 39.6 9 47.7Z"/></svg><span class="site-nav-brand-star" id="site-nav-brand-star"><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M12 1 L14.3 9.4 L23 12 L14.3 14.6 L12 23 L9.7 14.6 L1 12 L9.7 9.4 Z"/></svg></span></span>
         </a>
         <div class="site-nav-links">
           ${links.map(l=>`<a href="${l.href}"${isActive(l.href)?' class="active"':''}>${l.label}</a>`).join('')}
@@ -56,9 +56,14 @@
     .site-nav-brand:hover #site-nav-brand-word{color:#c11f1f;}
     #site-nav-brand-leaf{
       display:inline-block;height:0.67em;width:auto;
-      color:#c11f1f;margin-left:0;
+      color:#c11f1f;margin-left:0;position:relative;
     }
-    #site-nav-brand-leaf svg{display:block;height:100%;width:auto;}
+    #site-nav-brand-leaf > svg{display:block;height:100%;width:auto;}
+    #site-nav-brand-star{
+      position:absolute;top:-22%;right:2%;width:16%;height:16%;
+      color:#c11f1f;
+    }
+    #site-nav-brand-star svg{display:block;width:100%;height:100%;}
     .site-nav-links{
       display:flex;align-items:center;gap:18px;
       overflow-x:auto;-webkit-overflow-scrolling:touch;
